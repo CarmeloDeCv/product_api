fbadbc63f9787f5de772638af29fb406d1d4917a
Carmelo Simone Dec <icey@CarmelosMacBook.fritz.box>
2026-08-22 16:16:32 +0200

    added a login check to request or modify data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
 from pydantic import BaseModel
 from database import SessionLocal, engine, Base
 from sqlalchemy.orm import Session 
-from auth import hash_password, verify_password, create_access_token
+from auth import hash_password, verify_password, create_access_token, SECRET_KEY, ALGORITHM
 from models import ProductDB, UserDB, WorkerDB
 
 app = FastAPI()
@@ -15,6 +17,8 @@ def get_db():
         yield db
     finally:
         db.close()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 class Product(BaseModel):
     name:str
@@ -31,20 +35,31 @@ class Workers(BaseModel):
     surname:str
     role:str
 
+def get_current_user(token:str = Depends(oauth2_scheme), db:Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user = db.query(UserDB).filter(UserDB.name == username).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
+
 @app.get("/")
 def home():
     return {"message": "It works"}
 
 @app.get("/products")
-def list_products(db: Session = Depends(get_db)):
+def list_products(db: Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     return db.query(ProductDB).all()
 
 @app.get("/users")
-def list_users(db:Session = Depends(get_db)):
+def list_users(db:Session = Depends(get_db), current_user: UserDB=Depends(get_current_user)):
     return db.query(UserDB).all()
 
 @app.get("/workers")
-def list_workers(db:Session = Depends(get_db)):
+def list_workers(db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     return db.query(WorkerDB).all()
 
 @app.post("/register")
@@ -57,22 +72,22 @@ def register(username:str, password:str, db:Session = Depends(get_db)):
     return {"registered": new_user.name}
 
 @app.post("/login")
-def login(username:str, password:str, db:Session = Depends(get_db)):
-    user=db.query(UserDB).filter(UserDB.name==username).first()
-    if not user or not (verify_password(password, user.hashed_password)):
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db:Session = Depends(get_db)):
+    user=db.query(UserDB).filter(UserDB.name==form_data.username).first()
+    if not user or not (verify_password(form_data.password, user.hashed_password)):
         raise HTTPException(status_code=401, detail="Username or password not matching")
     token = create_access_token({"sub":user.name})
     return {"access_token": token, "token_type":"bearer"}
 
 @app.get("/products/{product_id}")
-def get_product(product_id: int, db:Session = Depends(get_db)):
+def get_product(product_id: int, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     db_product = db.query(ProductDB).filter(ProductDB.id==product_id).first()
     if db_product is None:
         raise HTTPException(status_code=404, detail="Product not found")
     return db_product
 
 @app.post("/products")
-def create_product(product:Product, db: Session = Depends(get_db)):
+def create_product(product:Product, db: Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     new_product = ProductDB(name=product.name, price=product.price, in_stock=product.in_stock)
     db.add(new_product)
     db.commit()
@@ -80,7 +95,7 @@ def create_product(product:Product, db: Session = Depends(get_db)):
     return new_product
 
 @app.put("/products/{product_id}")
-def update_product(product_id:int, product:Product, db:Session = Depends(get_db)):
+def update_product(product_id:int, product:Product, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     db_product = db.query(ProductDB).filter(ProductDB.id==product_id).first()
     if product_id is None:
         raise HTTPException(status_code=404, detail="Product not found")
@@ -91,7 +106,7 @@ def update_product(product_id:int, product:Product, db:Session = Depends(get_db)
     return {"updated":db_product.id, "product":product}
 
 @app.delete("/products/{product_id}")
-def delete_product(product_id: int, db:Session = Depends(get_db)):
+def delete_product(product_id: int, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     db_product = db.query(ProductDB).filter(ProductDB.id==product_id).first()
     if db_product is None:
         raise HTTPException(status_code=404, detail="Product not found")
@@ -100,14 +115,14 @@ def delete_product(product_id: int, db:Session = Depends(get_db)):
     return {"deleted": db_product}
 
 @app.get("/users/{user_id}")
-def get_user(user_id:int, db:Session = Depends(get_db)):
+def get_user(user_id:int, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     db_user = db.query(UserDB).filter(ProductDB.id == user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail = "User not found")
     return db_user
 
 @app.post("/users")
-def create_user(user: User, db: Session = Depends(get_db)):
+def create_user(user: User, db: Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     new_user = UserDB(name=user.name, surname = user.surname, phone_number = user.phone_number)
     db.add(new_user)
     db.commit()
@@ -115,7 +130,7 @@ def create_user(user: User, db: Session = Depends(get_db)):
     return new_user
 
 @app.put("/users/{user_id}")
-def change_user(user_id:int, user:User, db:Session = Depends(get_db)):
+def change_user(user_id:int, user:User, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     db_user = db.query(UserDB).filter(UserDB.id==user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -126,7 +141,7 @@ def change_user(user_id:int, user:User, db:Session = Depends(get_db)):
     return {"updated":db_user.id, "user": user}
 
 @app.delete("/users/{user_id}")
-def delete_user(user_id:int, db:Session = Depends(get_db)):
+def delete_user(user_id:int, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     db_user = db.query(UserDB).filter(UserDB.id==user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -135,14 +150,14 @@ def delete_user(user_id:int, db:Session = Depends(get_db)):
     return {"deleted":db_user}
 
 @app.get("/workers/{worker_id}")
-def get_worker(worker_id:int, db:Session = Depends(get_db)):
+def get_worker(worker_id:int, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     db_worker = db.query(WorkerDB).filter(WorkerDB.id == worker_id).first()
     if db_worker is None:
         raise HTTPException(status_code=404, detail="worker not found")
     return db_worker
 
 @app.post("/workers")
-def create_worker(worker:Workers, db:Session = Depends(get_db)):
+def create_worker(worker:Workers, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     new_worker = WorkerDB(name=worker.name, surname=worker.surname, role=worker.role)
     db.add(new_worker)
     db.commit()
@@ -150,7 +165,7 @@ def create_worker(worker:Workers, db:Session = Depends(get_db)):
     return new_worker
 
 @app.put("/workers/{worker_id}")
-def change_worker(worker_id:int, worker:Workers, db:Session = Depends(get_db)):
+def change_worker(worker_id:int, worker:Workers, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     db_worker = db.query(WorkerDB).filter(WorkerDB.id==worker_id).first()
     if db_worker is None:
         raise HTTPException(status_code=404, detail="worker not found")
@@ -161,7 +176,7 @@ def change_worker(worker_id:int, worker:Workers, db:Session = Depends(get_db)):
     return{"updated":db_worker.id, "worker":worker}
 
 @app.delete("/workers/{worker_id}")
-def delete_worker(worker_id:int, db:Session = Depends(get_db)):
+def delete_worker(worker_id:int, db:Session = Depends(get_db), current_user:UserDB=Depends(get_current_user)):
     db_worker = db.query(WorkerDB).filter(WorkerDB.id==worker_id).first()
     if db_worker is None:
         raise HTTPException(status_code=404, detail="worker not found")
